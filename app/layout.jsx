b820ca71bea7d6b9fdf32e92ed870b6a346671b8
jdodlsuw <jdodlsuw@gmail.com>
2025-07-28 09:31:10 +0700

    fix: refactor layout and implement session management with script loading

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,51 +1,13 @@
-"use client";
-
 import '../styles/globals.css';
 
+export const metadata = {
+    title: {
+        template: '%s | Netlify',
+        default: 'Netlify Starter'
+    }
+};
+
 export default function RootLayout({ children }) {
-    /**
-     * Load file
-     * @author NTSAN 08.06.2025
-     */
-    function _loadSource(tag, config) {
-        var el = document.createElement(tag);
-        if (config) {
-            for (let i in config) {
-                el[i] = config[i];
-            }
-        }
-        document.head.appendChild(el);
-    }
-
-    function getSessionUUID() {
-        // Định nghĩa key để lưu trong sessionStorage
-        const uuidKey = 'session_uuid';
-        // Thử lấy UUID từ sessionStorage
-        let sessionUUID = sessionStorage.getItem(uuidKey);
-        // Nếu chưa có UUID trong session này
-        if (!sessionUUID) {
-            // Tạo một UUID mới bằng API của trình duyệt
-            sessionUUID = crypto.randomUUID();
-
-            // Lưu UUID mới vào sessionStorage
-            sessionStorage.setItem(uuidKey, sessionUUID);
-        }
-        // Trả về UUID
-        return sessionUUID;
-    }
-
-    _loadSource('script', {
-        src: `https://testcdnamisapp.misacdn.net/support/libs/chat-customer-0.0.0/core.js`,
-        type: 'text/javascript',
-        onload: async () => {
-            window.initAmisSupport({
-                clientId: '1',
-                identityId: getSessionUUID(),
-                environment: 'TestOnline'
-            });
-        }
-    });
-
     return (
         <html lang="en">
             <head>
