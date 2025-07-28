859597ffaab37beb7dc732e472346c02df3fa234
jdodlsuw <jdodlsuw@gmail.com>
2025-07-28 09:22:04 +0700

    fix: implement session UUID management and load external scrip

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,6 +8,49 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+    /**
+     * Load file
+     * @author NTSAN 08.06.2025
+     */
+    function _loadSource(tag, config) {
+        var el = document.createElement(tag);
+        if (config) {
+            for (let i in config) {
+                el[i] = config[i];
+            }
+        }
+        document.head.appendChild(el);
+    }
+
+    function getSessionUUID() {
+        // Định nghĩa key để lưu trong sessionStorage
+        const uuidKey = 'session_uuid';
+        // Thử lấy UUID từ sessionStorage
+        let sessionUUID = sessionStorage.getItem(uuidKey);
+        // Nếu chưa có UUID trong session này
+        if (!sessionUUID) {
+            // Tạo một UUID mới bằng API của trình duyệt
+            sessionUUID = crypto.randomUUID();
+
+            // Lưu UUID mới vào sessionStorage
+            sessionStorage.setItem(uuidKey, sessionUUID);
+        }
+        // Trả về UUID
+        return sessionUUID;
+    }
+
+    _loadSource('script', {
+        src: `https://testcdnamisapp.misacdn.net/support/libs/chat-customer-0.0.0/core.js`,
+        type: 'text/javascript',
+        onload: async () => {
+            window.initAmisSupport({
+                clientId: '1',
+                identityId: getSessionUUID(),
+                environment: 'TestOnline'
+            });
+        }
+    });
+
     return (
         <html lang="en">
             <head>
@@ -19,7 +62,6 @@ export default function RootLayout({ children }) {
                         <main className="grow">{children}</main>
                     </div>
                 </div>
-                {/* <script type="module" src="script.js"></script> */}
             </body>
         </html>
     );
