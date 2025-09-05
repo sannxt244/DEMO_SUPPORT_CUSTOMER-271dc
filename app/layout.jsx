dfbf16daa2ed2626a0fedaf608cf56d8d4f3eaa7
jdodlsuw <jdodlsuw@gmail.com>
2025-09-05 18:22:12 +0700

    feat: trang chat khach hnag

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,10 @@
 import '../styles/globals.css';
+import Script from 'next/script';
 
 export const metadata = {
     title: {
-        template: '%s | Netlify',
-        default: 'Netlify Starter'
+        template: '%s | ',
+        default: 'AMIS Support'
     }
 };
 
@@ -13,13 +14,8 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
-            <body className="antialiased text-white bg-blue-900 min-h-screen">
-                <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
-                    <div className="flex flex-col w-full max-w-5xl mx-auto grow">
-                        <main className="grow">{children}</main>
-                    </div>
-                </div>
-            </body>
+            <body className="min-h-screen">{children}</body>
+            <Script src="https://testcdnamisapp.misacdn.net/support/core/1.0.0-testonline/core.js"></Script>
         </html>
     );
 }
