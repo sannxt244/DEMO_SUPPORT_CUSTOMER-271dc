99d34702ddfc26b4ff945640180d0059b89a0b1b
jdodlsuw <jdodlsuw@gmail.com>
2025-09-12 10:59:32 +0700

    fix: build

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 
-export default function RootLayout({ children }) {
+export default function RootLayout() {
     useEffect(() => {
         /**
          * Load file
